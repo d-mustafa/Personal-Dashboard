76330f08bb0979704178ea533591e9bb1173d8b4
Divine <160066288+d-mustafa@users.noreply.github.com>
2026-02-03 10:22:05 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+// PERSONAL DASHBOARD
+
+// Clock
+
+// Music
+let importMusicBtn = document.getElementById("music-btn");
+importMusicBtn.addEventListener("click", requestMP3File);
+
+function requestMP3File() {
+  console.log("gimme file");
+}
+
+// Drawing
