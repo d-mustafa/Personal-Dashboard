361f8a9741178e23d8c617ed06c0cc71f268fb45
Divine <160066288+d-mustafa@users.noreply.github.com>
2026-02-03 22:48:40 -0700

    Implement audio file input and playback
    Added functionality to handle audio file input and play it.

--- script.js
+++ script.js
@@ -3,11 +3,33 @@
 // Clock
 
 // Music
-let importMusicBtn = document.getElementById("music-btn");
-importMusicBtn.addEventListener("click", requestMP3File);
-
-function requestMP3File() {
-  console.log("gimme file");
+let fileInput = document.getElementById("file-input");
+function triggerFileInput() {
+  fileInput.click();
 }
 
+let audioPlayer = document.getElementById("audio-player");
+let audioFile;
+fileInput.addEventListener("change", (event) => {
+  const files = event.target.files;
+
+  if (files && files.length > 0) {
+    // Process the first selected file
+    const file = files[0];
+    console.log(`Audio file: ${file.name}, ${file.type}, ${file.size}bytes`);
+    
+    audioPlayer.src = URL.createObjectURL(audioFile);
+    audioPlayer.play();
+
+    // // Read the file into memory
+    // let reader = new FileReader();
+    // reader.onload = function(e) {
+    //   audioFile = new Audio(e.target.result);
+    //   audioPlayer.src = URL.createObjectURL(audioFile);
+    //   audioPlayer.play();
+    // }
+  }
+})
+
 // Drawing
+
