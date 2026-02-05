3c10d1ea331ffa7e40ce2e8c87ce0af8e3387986
Divine <160066288+d-mustafa@users.noreply.github.com>
2026-02-04 19:22:17 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,71 +1,110 @@
 // PERSONAL DASHBOARD
-
+console.log("fillRect")
 // Clock
 const outerClock = document.getElementById("clock-widget-container");
-const cnv = document.getElementById("clock");
-const ctx = cnv.getContext("2d");
+const clockCnv = document.getElementById("clock");
+const clockCtx = clockCnv.getContext("2d");
 
-// outerClock.addEventListener("mouseenter", clockify);
-// outerClock.addEventListener("mouseleave", unclockify);
-// function clockify() {
-//   cnv.style.borderRadius = "100rem";
-// }
-// function unclockify() {
-//   cnv.style.borderRadius = "0.5rem";
-// }
-
+outerClock.addEventListener("mouseenter", clockify);
+outerClock.addEventListener("mouseleave", unclockify);
+function clockify() {
+     clockCnv.style.borderRadius = "100rem";
+}
+function unclockify() {
+     clockCnv.style.borderRadius = "0.5rem";
+}
+let [secondHand, minuteHand, hourHand] = [0, 0, 0];
 function tick() {
-  ctx.clearRect(0, 0, cnv.width, cnv.height);
+    clockCtx.clearRect(0, 0, clockCnv.width, clockCnv.height);
 
-  // clocks center
-  ctx.fillStyle = "black";
-  ctx.beginPath();
-  ctx.arc(cnv.width/2, cnv.height/2, 5, Math.PI * 2, 0);
-  ctx.fill();
-
-  ctx.fillStyle = "white";
-  ctx.beginPath();
-  ctx.arc(cnv.width/2, cnv.height/2, 2.5, Math.PI * 2, 0);
-  ctx.fill();
-
-  
-  ctx.fillStyle = "black"
-  // second hand
-  ctx.rect(cnv.width/2, cnv.height/2, 100, 2);
-
-  // minute hand
-  ctx.rect(cnv.width/2, cnv.height/2, 100, 5);
-  
-  // hour hand
-  ctx.rect(cnv.width/2, cnv.height/2, 80, 5);
-
-  
-  requestAnimationFrame(tick);
+    // clocks center
+    clockCtx.fillStyle = "black";
+    clockCtx.beginPath();
+    clockCtx.arc(clockCnv.width/2, clockCnv.height/2, 5, Math.PI * 2, 0);
+    clockCtx.fill();
+    
+    clockCtx.fillStyle = "white";
+    clockCtx.beginPath();
+    clockCtx.arc(clockCnv.width/2, clockCnv.height/2, 2.5, Math.PI * 2, 0);
+    clockCtx.fill();
+    
+      
+    clockCtx.fillStyle = "black";
+    // second hand
+    clockCtx.fillRect(clockCnv.width/2, clockCnv.height/2, 100, 2);
+    
+    // minute hand
+    clockCtx.fillRect(clockCnv.width/2, clockCnv.height/2, 100, 5);
+      
+    // hour hand
+    clockCtx.fillRect(clockCnv.width/2, clockCnv.height/2, 80, 5);
+    
+      
+    requestAnimationFrame(tick);
 }
 tick();
 
 // Music
 let fileInput = document.getElementById("file-input");
 function triggerFileInput() {
-  fileInput.click();
+    fileInput.click();
 }
 
 const audioPlayer = document.getElementById("audio-player");
 let audioFile;
 fileInput.addEventListener("change", (event) => {
-  const files = event.target.files;
-
-  if (files && files.length > 0) {
-    // Process the first selected file
-    const file = files[0];
-    console.log(`Audio file: ${file.name}, ${file.type}, ${file.size}bytes`);
-
-    audioPlayer.src = URL.createObjectURL(file);
-    audioPlayer.play();
-  }
+    const files = event.target.files;
+    
+    if (files && files.length > 0) {
+        // Process the first selected file
+        const file = files[0];
+        console.log(`Audio file: ${file.name}, ${file.type}, ${file.size}bytes`);
+        
+        audioPlayer.src = URL.createObjectURL(file);
+        audioPlayer.play();
+    }
 });
 
 // Drawing
+const drawCnv = document.getElementById("drawing");
+const drawCtx = drawCnv.getContext("2d")
+const clearBtn = document.getElementById("clear-drawing");
+let clearCanvas = false;
+clearBtn.addEventListener("click", () => {
+    clearCanvas = true;
+})
 
+let draw = false;
+let mouseX, mouseY;
+drawCnv.addEventListener("mousedown", () => {
+    draw = true;
+})
+drawCnv.addEventListener("mouseip", () => {
+    draw = false;
+})
 
+cnv.addEventListener("mousemove", (event) => {
+    let rect = cnv.getBoundingClientRect();
+    console.log(event.clientX-rect.left, " ", event.clientY-rect.top)
+})
 
+drawCnv.addEventListener("mousemove", (event) => {
+    let rect = drawCnv.getBoundingClientRect();
+    [mouseX, mouseY] = [event.clientX-rect.left, event.clientY-rect.top];
+})
+
+function draw() {
+    if (clearCanvas) {
+        drawCtx.clearRect(0, 0, drawCnv.width, drawCnv.height);
+        clearCanvas = false;
+    }
+    if (draw) {
+        drawCtx.fillStyle = "black";
+        drawCtx.beginPath();
+        drawCtx.arc(mouseX, mouseY, 2.5, Math.PI * 2, 0);
+        drawCtx.fill();
+    }
+  
+    requestAnimationFrame(draw);
+}
+draw();
