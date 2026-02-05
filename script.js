1c85c3b9af206f621b44cfeba81482654ed03b1d
Divine <160066288+d-mustafa@users.noreply.github.com>
2026-02-05 12:21:59 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,36 @@
 // PERSONAL DASHBOARD
 
+// Mouse
+let mouseDown = false;
+document.addEventListener("mousedown", () => {
+  mouseDown = true;
+});
+document.addEventListener("mouseup", () => {
+  mouseDown = false;
+});
+
+let [clockMouseX, clockMouseY] = [0, 0];
+let [drawMouseX, drawMouseY] = [0, 0];
+let lastX, lastY;
+document.addEventListener("mousemove", (event) => {
+  lastX = drawMouseX;
+  lastY = drawMouseY;
+
+  // clock canvas
+  const clockRect = clockCnv.getBoundingClientRect();
+  const clockScaleX = clockCnv.width / clockRect.width;
+  const clockScaleY = clockCnv.height / clockRect.height;
+  clockMouseX = (event.clientX - clockRect.left) * clockScaleX;
+  clockMouseY = (event.clientY - clockRect.top) * clockScaleY;
+
+  // draw canvas
+  const drawRect = drawCnv.getBoundingClientRect();
+  const drawScaleX = drawCnv.width / drawRect.width;
+  const drawScaleY = drawCnv.height / drawRect.height;
+  drawMouseX = (event.clientX - drawRect.left) * drawScaleX;
+  drawMouseY = (event.clientY - drawRect.top) * drawScaleY;
+});
+
 // Clock
 const clockCnv = document.getElementById("clock");
 const clockCtx = clockCnv.getContext("2d");
@@ -10,6 +41,7 @@ let seconds = time.getSeconds();
 let minutes = time.getMinutes();
 let hours = time.getHours();
 
+// calculates second, minute, and hour hand positions
 let secondHand = -Math.PI + ((2 * Math.PI) / 60) * seconds;
 let minuteHand =
   -Math.PI + ((2 * Math.PI) / 60) * minutes + ((2 * Math.PI) / 3600) * seconds;
@@ -20,17 +52,19 @@ let hourHand =
   (Math.PI / 12) * (seconds / 3600);
 
 let [secondTick, minuteTick, hourTick] = [Date.now(), Date.now(), Date.now()];
-let tickMark = 0;
+let tickMark = ((2 * Math.PI) / 60) * 8.5;
+let interactSpeed = 1;
 function tick() {
   clockCtx.clearRect(0, 0, clockCnv.width, clockCnv.height);
 
   // clocks tick marks
-  for (let i = 1; i < 60; i++) {
+  for (let i = 1; i < 61; i++) {
     clockCtx.fillStyle = "black";
     clockCtx.save();
     clockCtx.translate(clockCnv.width / 2, clockCnv.height / 2);
     clockCtx.rotate(tickMark);
-    if (i % 5 === 0 || i === 1) {
+    if (i % 5 === 0) {
+      if (i === 60) clockCtx.fillStyle = "#51a2ff";
       clockCtx.fillRect(
         -(clockCnv.width / 2) + 48,
         -(clockCnv.height / 2) + 48,
@@ -46,9 +80,9 @@ function tick() {
       );
     }
     clockCtx.restore();
-    tickMark += (2 * Math.PI) / 59;
+    tickMark += (2 * Math.PI) / 60;
   }
-  tickMark = 0;
+  tickMark = ((2 * Math.PI) / 60) * 8.5;
 
   // clocks bottom
   clockCtx.fillStyle = "black";
@@ -64,7 +98,7 @@ function tick() {
   clockCtx.rotate(secondHand);
   clockCtx.fillRect(-1, -45, 2, 180);
   clockCtx.restore();
-  if (now - secondTick >= 1000) {
+  if (now - secondTick >= 1000 * interactSpeed) {
     secondHand += (2 * Math.PI) / 60; // 60 seconds for a full revolution
     secondTick = Date.now();
   }
@@ -76,8 +110,8 @@ function tick() {
   clockCtx.rotate(minuteHand);
   clockCtx.fillRect(-2.5, -25, 4, 145);
   clockCtx.restore();
-  if (now - minuteTick >= 1000 * (60 / 6)) {
-    minuteHand += (2 * Math.PI) / (60 * 6); // 60 minutes for a full revolution
+  if (now - minuteTick >= 1000 * interactSpeed) {
+    minuteHand += (2 * Math.PI) / 3600; // 60 minutes for a full revolution (3600 seconds)
     minuteTick = Date.now();
   }
 
@@ -87,8 +121,8 @@ function tick() {
   clockCtx.rotate(hourHand);
   clockCtx.fillRect(-2.5, -20, 5, 120);
   clockCtx.restore();
-  if (now - hourTick >= 1000) {
-    hourHand += (2 * Math.PI) / 3600; // 3600 seconds for a full revolution
+  if (now - hourTick >= 1000 * interactSpeed) {
+    hourHand += (2 * Math.PI) / 43200; // 12 hours for a full revolution (43200 seconds)
     hourTick = Date.now();
   }
 
@@ -97,6 +131,8 @@ function tick() {
   clockCtx.beginPath();
   clockCtx.arc(clockCnv.width / 2, clockCnv.height / 2, 2.5, Math.PI * 2, 0);
   clockCtx.fill();
+
+  // clock interactions
 
   requestAnimationFrame(tick);
 }
@@ -140,48 +176,28 @@ clearBtn.addEventListener("click", () => {
   clearCanvas = true;
 });
 
-let drawEnabled = false;
-document.addEventListener("mousedown", () => {
-  drawEnabled = true;
-});
-document.addEventListener("mouseup", () => {
-  drawEnabled = false;
-});
-
-let [mouseX, mouseY] = [0, 0];
-let lastX, lastY;
-document.addEventListener("mousemove", (event) => {
-  lastX = mouseX;
-  lastY = mouseY;
-  const rect = drawCnv.getBoundingClientRect();
-  const scaleX = drawCnv.width / rect.width;
-  const scaleY = drawCnv.height / rect.height;
-  mouseX = (event.clientX - rect.left) * scaleX;
-  mouseY = (event.clientY - rect.top) * scaleY;
-});
-
 function draw() {
   if (clearCanvas) {
     drawCtx.clearRect(0, 0, drawCnv.width, drawCnv.height);
     clearCanvas = false;
-    [mouseX, mouseY, lastX, lastY] = [0, 0, 0, 0];
+    [drawMouseX, drawMouseY, lastX, lastY] = [0, 0, 0, 0];
   }
   let mouseInCanvas =
-    mouseX > 0 &&
-    mouseY > 0 &&
-    mouseX < drawCnv.width &&
-    mouseY < drawCnv.height;
-  if (drawEnabled && mouseInCanvas) {
+    drawMouseX > 0 &&
+    drawMouseY > 0 &&
+    drawMouseX < drawCnv.width &&
+    drawMouseY < drawCnv.height;
+  if (mouseDown && mouseInCanvas) {
     drawCtx.fillStyle = "black";
     drawCtx.beginPath();
     drawCtx.arc(lastX, lastY, 5, Math.PI * 2, 0);
     drawCtx.fill();
 
     drawCtx.beginPath();
-    drawCtx.arc(mouseX, mouseY, 5, Math.PI * 2, 0);
+    drawCtx.arc(drawMouseX, drawMouseY, 5, Math.PI * 2, 0);
     drawCtx.fill();
   }
-  if (!mouseInCanvas) [mouseX, mouseY, lastX, lastY] = [0, 0, 0, 0];
+  if (!mouseInCanvas) [drawMouseX, drawMouseY, lastX, lastY] = [0, 0, 0, 0];
 
   requestAnimationFrame(draw);
 }
